514f25b9dd0938268fbd759bdb5c9fe907debd7c
Jhay1212 <jhayjane78@gmail.com>
2023-06-17 17:38:36 +0800

    Added double jump functionality
    Added a code where it let the player double jump when possible.

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -3,10 +3,11 @@ extends CharacterBody2D
 
 @export var SPEED: float = 300.0
 @export var JUMP_VELOCITY: float = -200.0
+@export var double_jump_velocity: float = -100
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var has_double_jumped: bool = false
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,8 +15,14 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			has_double_jumped = false
+		elif not has_double_jumped:
+			velocity.y = double_jump_velocity
+			has_double_jumped = true
+			
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
