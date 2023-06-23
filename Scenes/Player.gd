458a69bc5919680b809982560e7cc34bba4fd338
Jhay1212 <jhayjane78@gmail.com>
2023-06-23 17:51:29 +0800

    Tried fixing jump animation

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -11,6 +11,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var has_double_jumped: bool = false
 var is_locked = false
 var direction: Vector2 = Vector2.ZERO
+var was_in_air: bool = false
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -22,9 +23,15 @@ func _physics_process(delta):
 		if is_on_floor():
 			velocity.y = JUMP_VELOCITY
 			has_double_jumped = false
+			jump()
+			
+			if was_in_air == true:
+				land()
+				
 		elif not has_double_jumped:
 			velocity.y = double_jump_velocity
 			has_double_jumped = true
+
 			
 
 	# Get the input direction and handle the movement/deceleration.
@@ -37,6 +44,7 @@ func _physics_process(delta):
 	move_and_slide()
 	update_animation()
 	change_direction()
+	
 
 
 func update_animation():
@@ -54,3 +62,13 @@ func change_direction():
 		animated_sprite.flip_h = true
 	elif direction.x > 0:
 		animated_sprite.flip_h = false
+
+
+func jump():
+	velocity.y += direction.y
+	animated_sprite.play('jump_start')
+	is_locked = true
+func land():
+	animated_sprite.play("jumo_Pend")
+	is_locked = true
+	pass
