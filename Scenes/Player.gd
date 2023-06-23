93323127f911040be28afe4b9262f8b53705b2a4
Jhay1212 <jhayjane78@gmail.com>
2023-06-23 17:57:28 +0800

    Finished fixing jump animation

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -17,6 +17,7 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		was_in_air = true
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump"):
